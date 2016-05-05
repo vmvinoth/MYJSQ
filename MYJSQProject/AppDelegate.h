db45bc0f07ea801446d8a812f793b96ca3d0d89e
Vinoth <vinoth@muralis-macbook-pro.local>
2016-05-05 13:57:04 +0530

    Initial Commit

---
+++ MYJSQProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MYJSQProject
+//
+//  Created by Vinoth on 5/5/16.
+//  Copyright © 2016 Myantra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MYJSQProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MYJSQProject
+//
+//  Created by Vinoth on 5/5/16.
+//  Copyright © 2016 Myantra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
